192daa5b78097ebd288d8ec223b6129804862875
ValeriyGuleenkova <64977472+ValeriyGuleenkova@users.noreply.github.com>
2023-04-11 23:13:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,19 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from statsmodels.stats.proportion import proportions_ztest
 
+chat_id = 215606022
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
+#x - контроль, y - тест
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    _,p_val = proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'smaller')
+    sgn_lvl = 0.08
+    print(p_val)
+    #если p_value маленькое, то отвергаем гипотезу, то есть на тесте лучше
+    #вопрос в задании "отклонить ли гипотезу "на тесте лучше" "
+    if(p_val < sgn_lvl): #при таком знаке 1/3, при противоположном 2/3
+        return False
+    return True 
